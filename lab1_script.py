a5fd14d90b89ec6ffefca88bd003deae40ff515d
Olivier Vadiavaloo <vadiaval@ualberta.ca>
2021-09-02 22:12:44 -0600

    Download and print itself

--- lab1_script.py
+++ lab1_script.py
@@ -7,4 +7,9 @@ print()
 # Send GET request to Google homepage, print response text
 res = requests.get("http://www.google.com")
 print("response text for GET request to Google homepage:", end="\n\n")
+print(res.text, end="\n\n")
+
+# Download itself from Github and print its own source code
+res = requests.get("https://raw.githubusercontent.com/olivier-vadiaval/CMPUT404Fall2021-Lab1/main/lab1_script.py")
+print("Python script source code from Github:", end="\n\n")
 print(res.text)
